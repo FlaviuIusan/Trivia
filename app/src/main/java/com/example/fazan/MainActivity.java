544a716fdc3e3d1c05651d135fb6c9b8178e3dc8
Flavius890 <65131626+Flavius890@users.noreply.github.com>
2020-06-01 18:20:48 +0300

    Am facut design-ul la MainActivity si PlayActivity.

--- app/src/main/java/com/example/fazan/MainActivity.java
+++ app/src/main/java/com/example/fazan/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fazan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        findViewById(R.id.buttonPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
